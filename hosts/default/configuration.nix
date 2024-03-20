45f4a77956ff2d96a1b1c25ca136f98aa49cbe6d
Daniel Gu <bobthebuilder10492@gmail.com>
2024-03-20 23:44:22 +0800

    add: fcitx5 [52]

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -48,6 +48,15 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+    ];
+};
+
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
