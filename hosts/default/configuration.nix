af7250509be79b0319942dbf37906a2618433ea4
Daniel Gu <bobthebuilder10492@gmail.com>
2024-04-04 12:17:16 +0800

    fix: chinese again [61]

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -51,10 +51,11 @@
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
-        fcitx5-rime
-        fcitx5-chinese-addons
+      fcitx5-chinese-addons
+      fcitx5-rime
+      fcitx5-hangul
     ];
-};
+  };
 
 
   # Enable the X11 windowing system.
