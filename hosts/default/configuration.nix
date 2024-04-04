d35238336fcc1e29eb53bccc73e88e1314210334
Daniel Gu <bobthebuilder10492@gmail.com>
2024-04-04 11:56:22 +0800

    fix: chinese input [59]

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -51,8 +51,8 @@
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
-        fcitx5-mozc
-        fcitx5-gtk
+        fcitx5-rime
+        fcitx5-chinese-addons
     ];
 };
 
