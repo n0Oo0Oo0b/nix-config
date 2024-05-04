2e254ea2b50beaa86dbfe88f3ccf42a9d90af1d7
Daniel Gu <bobthebuilder10492@gmail.com>
2024-05-04 11:47:45 +0800

    add: nix vscode extension [69 current  2024-05-04 11:47:44  24.05.20240224.73de017  6.1.79                          *]

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -48,10 +48,14 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
+      asvetliakov.vscode-neovim
+
+      bbenoist.nix
+
       james-yu.latex-workshop
       ms-python.python
+
       ms-python.vscode-pylance
-      asvetliakov.vscode-neovim
     ];
   };
 
