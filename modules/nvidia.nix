433b63c58fde92f6a716c825f23a174e1a1aa0b6
Daniel Gu <bobthebuilder10492@gmail.com>
2024-05-10 21:36:56 +0800

    xorg config [81 current  2024-05-10 21:36:54  24.05.20240224.73de017  6.1.79                          *]

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -6,7 +6,13 @@
   services.xserver = {
     enable = true;
     videoDrivers = ["nvidia"];
+    monitorSection = ''
+      ModelName    "AOC 27V5"
+      HorizSync     85.0 - 85.0
+      VertRefresh   48.0 - 75.0
+    '';
     screenSection = ''
+      Monitor      "Monitor[0]"
       Option       "metamodes" "DP-2: 1920x1080_75 +0+0 {rotation=left, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-4: 2560x1440_144 +1080+240 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
       Option       "AllowIndirectGLXProtocol" "off"
       Option       "TripleBuffer" "on"
