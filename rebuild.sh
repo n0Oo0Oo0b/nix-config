df9c16e15b3029bbffd91779ff3f5e098fa3176a
Daniel Gu <bobthebuilder10492@gmail.com>
2024-07-08 12:25:52 +0800

    auto push

--- rebuild.sh
+++ rebuild.sh
@@ -23,12 +23,16 @@ echo "NixOS Rebuilding..."
 # Rebuild, output simplified errors, log trackebacks
 sudo nixos-rebuild switch --flake .#default &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
 
-# Commit changes with generation metadata
-current=$(nixos-rebuild list-generations | grep current)
-git commit -am "$1 [$current]"
-
-popd
-
 # Notify all OK!
 echo "Rebuild OK!"
 # notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
+
+# Commit changes with generation metadata
+current=$(nixos-rebuild list-generations | grep current)
+git commit -am "$1 [$current]"
+git push
+
+echo "Push OK!"
+
+popd
+
