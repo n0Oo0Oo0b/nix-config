53beb7f9fe27c25d782253c2606865ae34763cb2
Daniel Gu <bobthebuilder10492@gmail.com>
2024-07-22 13:39:25 +0800

    [127 current  2024-07-22 13:39:17  24.11.20240719.1d9c2c9  6.6.41                          *]

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -3,14 +3,23 @@
   pkgs,
   ...
 }: {
+  hardware.graphics.enable = true;
   services.xserver = {
     enable = true;
     videoDrivers = ["nvidia"];
-    screenSection = ''
-      Option         "metamodes" "DP-2: 1920x1080_75 +0+0 {rotation=left, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-4: 2560x1440_144 +1080+240 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
-      Option         "AllowIndirectGLXProtocol" "off"
-      Option         "TripleBuffer" "on"
-    '';
-    exportConfiguration = true;
+    # screenSection = ''
+    #   Option         "metamodes" "DP-2: 1920x1080_75 +0+0 {rotation=left, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-4: 2560x1440_144 +1080+240 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+    #   Option         "AllowIndirectGLXProtocol" "off"
+    #   Option         "TripleBuffer" "on"
+    # '';
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
