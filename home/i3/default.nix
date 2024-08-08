de154d606d1264765416365a7a2aa720c3867a85
Daniel Gu <bobthebuilder10492@gmail.com>
2024-08-08 12:03:30 +0800

    [143 current  2024-08-08 12:03:27  24.11.20240804.cb9a96f  6.6.44                          *]

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -5,24 +5,12 @@
     windowManager.i3.config = {
       modifier = "Mod4";
       fonts = ["JetBrains Mono Nerd Font 11.0"];
-      startup = [
-        {command = "bash $HOME/.screenlayout/monitor.sh";}
-        {command = "noisetorch -i";}
-        {command = "openrgb -p default";}
-      ];
+      terminal = "kitty";
     };
-    # Path, relative to HOME, where Home Manager should write the X session script.
-    # and NixOS will use it to start xorg session when system boot up
-    scriptPath = ".xsession";
-  };
-
-  home.file = {
-    ".screenlayout/monitor.sh".text = ''
-      #!/bin/sh
-      LEFT='DP-2'
-      RIGHT='DP-3'
-      xrandr --output $RIGHT --primary --right-of $LEFT --mode 2560x1440 --rate 144 --output $LEFT --mode 1920x1080 --rate 75 --rotate left
-      xrandr --output $RIGHT --pos 1080x240
+    profileExtra = ''
+      bash ~/.screenlayout/monitor.sh
+      noisetorch -i
+      openrgb -p default
     '';
   };
 }
