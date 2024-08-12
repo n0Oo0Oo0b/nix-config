afabcf61ef56eca9fc98eea51be09b3735549916
Daniel Gu <bobthebuilder10492@gmail.com>
2024-08-12 11:51:42 +0800

    [189 current  2024-08-12 11:51:37  24.11.20240804.cb9a96f  6.6.44                          *]

--- home/i3/i3status-rust.nix
+++ home/i3/i3status-rust.nix
@@ -6,36 +6,41 @@
         {
           block = "disk_space";
           path = "/";
-          format = " $icon $available / $total left ";
+          format = " $icon $used/$total ";
           info_type = "available";
           interval = 60;
           warning = 20.0;
           alert = 10.0;
           alert_unit = "GB";
         }
+
         {
           block = "memory";
           format = " $icon $mem_used.eng(prefix:G)/$mem_total.eng(prefix:G) ";
           warning_mem = 70.0;
           critical_mem = 90.0;
         }
+
         {
           block = "cpu";
           format = " $icon $utilization ";
           format_alt = " $icon $barchart ";
           interval = 1;
         }
+
         {
           block = "temperature";
           interval = 10;
           format = " $icon $max ";
         }
+
         {
           block = "nvidia_gpu";
+          format = " $icon $utilization $temperature ";
         }
-        {
-          block = "net";
-        }
+
+        {block = "net";}
+
         {
           block = "sound";
           driver = "pulseaudio";
@@ -45,18 +50,18 @@
             "alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1.3" = "HDMI";
           };
         }
+
         {
           block = "time";
           interval = 1;
           format = " $icon $timestamp.datetime(f:'%a %Y-%m-%d %T') ";
         }
-        {
-          block = "watson";
-          show_time = true;
-        }
+
+        {block = "watson";}
+
         {
           block = "menu"; # System menu
-          text = "\uf011";
+          text = "\\uf011";
           items = [
             {
               display = " Sleep ";
