fd1538b0b9077cf65d78d9998da754fad20524ce
Daniel Gu <bobthebuilder10492@gmail.com>
2024-08-13 22:53:49 +0800

    [202 current  2024-08-13 22:53:45  24.11.20240804.cb9a96f  6.6.44                          *]

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -8,78 +8,120 @@
     ./i3status-rust.nix
   ];
 
-  xsession = {
-    enable = true;
+  xsession.enable = true;
+  xsession.initExtra = ''
+    LEFT='DP-2'
+    RIGHT='DP-4'
+    ${pkgs.xorg.xrandr}/bin/xrandr --output $RIGHT --primary --right-of $LEFT --mode 2560x1440 --rate 144 --output $LEFT --mode 1920x1080 --rate 75 --rotate left
+    ${pkgs.xorg.xrandr}/bin/xrandr --output $RIGHT --pos 1080x240
+  '';
 
-    windowManager.i3 = {
-      enable = true;
-      config = {
-        modifier = "Mod4";
-        fonts = {
-          names = ["JetBrains Mono Nerd Font"];
-          style = "Regular";
-          size = 11.0;
-        };
-        terminal = "kitty";
+  xsession.windowManager.i3.enable = true;
+  xsession.windowManager.i3.config = {
+    modifier = "Mod4";
+    fonts = {
+      names = ["JetBrains Mono Nerd Font"];
+      style = "Regular";
+      size = 11.0;
+    };
+    terminal = "kitty";
 
-        gaps.inner = 10;
-        gaps.outer = 5;
+    gaps.inner = 10;
+    gaps.outer = 5;
 
-        keybindings = let
-          pactl = "${pkgs.pulseaudio}/bin/pactl";
-          playerctl = "${pkgs.playerctl}/bin/playerctl";
-          mod = config.xsession.windowManager.i3.config.modifier;
-        in
-          lib.mkOptionDefault {
-            "XF86AudioRaiseVolume" = "exec --no-startup-id ${pactl} set-sink-volume 0 +5%";
-            "XF86AudioLowerVolume" = "exec --no-startup-id ${pactl} set-sink-volume 0 -5%";
-            "XF86AudioMute" = "exec --no-startup-id ${pactl} set-sink-mute 0 toggle";
+    keybindings = let
+      pactl = "${pkgs.pulseaudio}/bin/pactl";
+      playerctl = "${pkgs.playerctl}/bin/playerctl";
+      mod = config.xsession.windowManager.i3.config.modifier;
+    in
+      lib.mkOptionDefault {
+        "XF86AudioRaiseVolume" = "exec --no-startup-id ${pactl} set-sink-volume 0 +5%";
+        "XF86AudioLowerVolume" = "exec --no-startup-id ${pactl} set-sink-volume 0 -5%";
+        "XF86AudioMute" = "exec --no-startup-id ${pactl} set-sink-mute 0 toggle";
 
-            "XF86AudioPlay" = "exec ${playerctl} play";
-            "XF86AudioPause" = "exec ${playerctl} pause";
-            "XF86AudioNext" = "exec ${playerctl} next";
-            "XF86AudioPrev" = "exec ${playerctl} previous";
+        "XF86AudioPlay" = "exec ${playerctl} play";
+        "XF86AudioPause" = "exec ${playerctl} pause";
+        "XF86AudioNext" = "exec ${playerctl} next";
+        "XF86AudioPrev" = "exec ${playerctl} previous";
 
-            "${mod}+i" = "focus up";
-            "${mod}+shift+i" = "move up";
-            "${mod}+j" = "focus left";
-            "${mod}+shift+j" = "move left";
-            "${mod}+k" = "focus down";
-            "${mod}+shift+k" = "move down";
-            "${mod}+l" = "focus right";
-            "${mod}+shift+l" = "move right";
-          };
-
-        bars = [
-          {
-            position = "bottom";
-            statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-bottom.toml";
-          }
-        ];
+        "${mod}+i" = "focus up";
+        "${mod}+shift+i" = "move up";
+        "${mod}+j" = "focus left";
+        "${mod}+shift+j" = "move left";
+        "${mod}+k" = "focus down";
+        "${mod}+shift+k" = "move down";
+        "${mod}+l" = "focus right";
+        "${mod}+shift+l" = "move right";
       };
 
-      extraConfig = ''
-        for_window [title="^zoom\s?$"] kill
-        for_window [title="^join\?action=join.*$"] kill
+    startup = let
+      no-notif = cmd: {
+        command = cmd;
+        notification = false;
+      };
+    in [
+      (no-notif "${pkgs.picom}/bin/picom -b")
+      (no-notif "${pkgs.flameshot}/bin/flameshot")
+      (no-notif "fxitc5 -d")
+      (no-notif "${pkgs.feh}/bin/feh --bg-fill $HOME/.background-image" // {always = true;})
+      (no-notif "${pkgs.noisetorch}/bin/noisetorch -i")
+      (no-notif "${pkgs.openrgb}/bin/openrgb -p default")
+    ];
 
-        exec_always --no-startup-id ${pkgs.feh}/bin/feh --bg-fill $HOME/.background-image
-        exec --no-startup-id ${pkgs.picom}/bin/picom -b
-        exec --no-startup-id ${pkgs.flameshot}/bin/flameshot
-        exec --no-startup-id fcitx5 -d
-      '';
-    };
+    bars = [
+      {
+        position = "bottom";
+        statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-bottom.toml";
+        trayOutput = "primary";
+        # Catppuccin-mocha
+        colors = let
+          crust = "#11111b";
+          mantle = "#181825";
+          base = "#1e1e2e";
+          surface0 = "#313244";
+          text = "#cdd6f4";
+          blue = "#89b4fa";
+          red = "#f38ba8";
 
-    initExtra = ''
-      LEFT='DP-2'
-      RIGHT='DP-4'
-      ${pkgs.xorg.xrandr}/bin/xrandr --output $RIGHT --primary --right-of $LEFT --mode 2560x1440 --rate 144 --output $LEFT --mode 1920x1080 --rate 75 --rotate left
-      ${pkgs.xorg.xrandr}/bin/xrandr --output $RIGHT --pos 1080x240
-    '';
-    profileExtra = ''
-      ${pkgs.noisetorch}/bin/noisetorch -i
-      ${pkgs.openrgb}/bin/openrgb -p default
-    '';
+          bg-text = bg: text: {
+            background = bg;
+            border = bg;
+            text = text;
+          };
+        in {
+          background = mantle;
+          activeWorkspace = bg-text blue crust;
+          focusedWorkspace = bg-text surface0 text;
+          inactiveWorkspace = bg-text base text;
+          urgentWorkspace = bg-text red crust;
+        };
+      }
+    ];
+
+    workspaceOutputAssign = let
+      left = "DP-2";
+      right = "DP-4";
+      ws = n: output: {
+        workspace = "workspace number ${toString n}";
+        output = output;
+      };
+    in [
+      (ws 1 left)
+      (ws 2 right)
+      (ws 3 left)
+      (ws 4 right)
+      (ws 5 left)
+      (ws 6 right)
+      (ws 7 left)
+      (ws 8 right)
+      (ws 9 left)
+      (ws 10 right)
+    ];
   };
+  xsession.windowManager.i3.extraConfig = ''
+    for_window [title="^zoom\s?$"] kill
+    for_window [title="^join\?action=join.*$"] kill
+  '';
 
   services.picom = {
     enable = true;
