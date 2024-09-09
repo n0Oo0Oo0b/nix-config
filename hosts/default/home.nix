c72d52a0b0ed36ccbe40c76bc30d90362a1f89a9
Daniel Gu <bobthebuilder10492@gmail.com>
2024-09-09 23:43:22 +0800

    [244 current  2024-09-09 23:43:19  24.11.20240906.574d1ea  6.6.49                          *]

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -22,8 +22,19 @@
   # NOTE: Check home-manager release notes before changing
   home.stateVersion = "23.11";
 
-  catppuccin.enable = true;
-  catppuccin.flavor = "mocha";
+  nix.settings = {
+    keep-outputs = true;
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.cudaSupport = true;
@@ -55,6 +66,7 @@
     ripgrep
     du-dust
     mprocs
+    neovim
 
     # Misc
     ffmpeg
@@ -62,7 +74,6 @@
     texliveFull
     alejandra
     pulseaudio
-    neovim
     osu-lazer-bin
 
     # Scripts
@@ -79,6 +90,9 @@
       fi
     '')
   ];
+
+  catppuccin.enable = true;
+  catppuccin.flavor = "mocha";
 
   gtk = {
     enable = true;
