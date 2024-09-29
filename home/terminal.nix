de7225ea67a19ad0f7fe8f51ab966c46e5b08ddb
Daniel Gu <bobthebuilder10492@gmail.com>
2024-09-29 11:59:56 +0800

    [273 current  2024-09-29 11:59:53  24.11.20240921.9357f4f  6.6.52                          *]

--- home/terminal.nix
+++ home/terminal.nix
@@ -32,10 +32,23 @@ in rec {
       (name: value: "$env.${name} = \"${toString value}\"")
       home.sessionVariables
     );
-    # Hardcoded zellij integration
+    extraConfig = ''
+      def start_zellij [] {
+        if 'ZELLIJ' not-in ($env | columns) {
+          if 'ZELLIJ_AUTO_ATTACH' in ($env | columns) and $env.ZELLIJ_AUTO_ATTACH == 'true' {
+            zellij attach -c
+          } else {
+            zellij
+          }
+
+          if 'ZELLIJ_AUTO_EXIT' in ($env | columns) and $env.ZELLIJ_AUTO_EXIT == 'true' {
+            exit
+          }
+        }
+      }
+    '';
     loginFile.text = ''
-      zellij attach -c main
-      exit
+      start_zellij
     '';
   };
 
