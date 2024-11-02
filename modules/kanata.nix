70d027f2e69e9dfdbdaff42386bfca36ea6f2795
Daniel Gu <bobthebuilder10492@gmail.com>
2024-11-02 10:25:34 +0800

    [341 current  2024-11-02 10:25:28  24.11.20241025.18536bf  6.6.58                          *]

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -15,7 +15,7 @@
         ];
         common = ''
           comp rmet
-          caps (multi f24 (tap-hold-press 300 100 esc lctl))
+          caps (multi f24 (tap-hold-press 0 200 esc lctl))
           lctl (multi ctl (layer-while-held ctrl))
           rctl (multi ctl (layer-while-held ctrl))
         '';
