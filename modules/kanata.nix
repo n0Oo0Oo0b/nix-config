e165e1a7400d8fd5267d7d641ac07011e5aac84a
Daniel Gu <bobthebuilder10492@gmail.com>
2024-11-02 12:08:36 +0800

    [343 current  2024-11-02 12:08:31  24.11.20241025.18536bf  6.6.58                          *]

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -3,45 +3,45 @@
     enable = true;
     keyboards.drunkdeer = {
       config = ''
-          (defsrc
-            caps comp
-            i j k l
-            w a s d q e r f
-          )
-
-          (defvar
-            ;; Values
-            mouse-speed 5
-            scroll-rate 50
-          )
-          (defalias
-            msu (movemouse-up 1 $mouse-speed)
-            msd (movemouse-down 1 $mouse-speed)
-            msl (movemouse-left 1 $mouse-speed)
-            msr (movemouse-right 1 $mouse-speed)
-            mwu (mwheel-up $scroll-rate 120)
-            mwd (mwheel-down $scroll-rate 120)
+        (defsrc
+          caps scrlck
+          i j k l
+          w a s d q e r f
         )
 
-          (deflayermap (default)
-            comp (layer-switch nav)
-            caps (multi f24 (tap-hold-press 0 200 esc lctl))
-          )
+        (defvar
+          ;; Values
+          mouse-speed 5
+          scroll-rate 50
+        )
+        (defalias
+          msu (movemouse-up 1 $mouse-speed)
+          msd (movemouse-down 1 $mouse-speed)
+          msl (movemouse-left 1 $mouse-speed)
+          msr (movemouse-right 1 $mouse-speed)
+          mwu (mwheel-up $scroll-rate 120)
+          mwd (mwheel-down $scroll-rate 120)
+        )
 
-          (deflayermap (nav)
-            ;; Arrow keys
-            i up
-            j left
-            k down
-            l right
-            ;; Mouse
-            q mlft
-            e mrgt
-            w @msu a @msl s @msd d @msr
-            r @mwu f @mwd
+        (deflayermap (default)
+          scrlck (layer-switch nav)
+          caps (multi f24 (tap-hold-press 0 200 esc lctl))
+        )
 
-            ret (layer-switch default)
-          )
+        (deflayermap (nav)
+          ;; Arrow keys
+          i up
+          j left
+          k down
+          l right
+          ;; Mouse
+          q mlft
+          e mrgt
+          w @msu a @msl s @msd d @msr
+          r @mwu f @mwd
+
+          ret (layer-switch default)
+        )
       '';
       extraDefCfg = ''
         process-unmapped-keys yes
