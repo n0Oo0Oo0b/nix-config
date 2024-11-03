f0a1824a744063e0fec7285cbe4e2dcbbd26fb13
Daniel Gu <bobthebuilder10492@gmail.com>
2024-11-03 21:44:42 +0800

    [355 current  2024-11-03 21:44:38  24.11.20241025.18536bf  6.6.58                          *]

--- home/i3/i3status-rust.nix
+++ home/i3/i3status-rust.nix
@@ -84,7 +84,6 @@
           block = "custom";
           command = "nc 127.0.0.1 60001 | ${pkgs.jq}/bin/jq .LayerChange.new";
           persistent = true;
-          icon = "\uf11c";
         }
 
         {
