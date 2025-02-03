aca70652421615f78c69571896bbc8c68930c9b5
Daniel Gu <bobthebuilder10492@gmail.com>
2025-02-03 15:15:22 +0800

    [392 current  2025-02-03 15:15:20  25.05.20250201.3a22805  6.12.11                          *]

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -13,6 +13,7 @@
 
   nix.settings = {
     substituters = [
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://cuda-maintainers.cachix.org"
@@ -22,6 +23,16 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
+    builders-use-substitutes = true;
+
+    experimental-features = ["nix-command" "flakes"];
+    auto-optimise-store = true;
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 3d";
   };
 
   nixpkgs.config.allowUnfree = true;
@@ -37,16 +48,6 @@
     options v4l2loopback devices=1 video_nr=1 card_label="OBS VCam" exclusive_caps=1
   '';
   security.polkit.enable = true;
-
-  nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
-    auto-optimise-store = true;
-  };
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 3d";
-  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
