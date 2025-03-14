1282e303ec3297acc4db116869783a482af8a7ff
Daniel Gu <bobthebuilder10492@gmail.com>
2025-03-14 19:45:28 +0800

    [419 current  2025-03-13 23:20:56  25.05.20250309.e3e32b6  6.12.18                          *]

--- modules/kanata/default.nix
+++ modules/kanata/default.nix
@@ -1,34 +1,27 @@
 { lib, pkgs, ... }: let
   configFile = import ./config_file.nix { inherit lib pkgs; };
 in {
-  config = lib.mkMerge [
-    # NixOS module
-    # (lib.optionalAttrs pkgs.stdenv.isLinux {
-    #   services.kanata = {
-    #     enable = true;
-    #     keyboards.drunkdeer = {
-    #       # devices = [
-    #       #   "/dev/input/by-id/usb-Drunkdeer_Drunkdeer_G60_ANSI_RYMicro-event-kbd"
-    #       #   "/dev/input/by-id/usb-Drunkdeer_Drunkdeer_G60_ANSI_RYMicro-if01-event-kbd"
-    #       # ];
-    #       port = 60001;
-    #       inherit configFile;
-    #     };
-    #   };
-    # })
-
-    # darwin launchd agent
-    {
-      services.karabiner-elements.enable = true;
-
-      launchd.agents.kanata = {
-        command = ''
-          sudo kanata \
-            --cfg ${configFile} \
-            --port 60001
-        '';
-        path = [pkgs.kanata];
-      };
-    }
-  ];
+  services.kanata = {
+    enable = true;
+    keyboards.drunkdeer = {
+      # devices = [
+      #   "/dev/input/by-id/usb-Drunkdeer_Drunkdeer_G60_ANSI_RYMicro-event-kbd"
+      #   "/dev/input/by-id/usb-Drunkdeer_Drunkdeer_G60_ANSI_RYMicro-if01-event-kbd"
+      # ];
+      port = 60001;
+      inherit configFile;
+    };
+  };
 }
+# {
+#   services.karabiner-elements.enable = true;
+#
+#   launchd.agents.kanata = {
+#     command = ''
+#       sudo kanata \
+#         --cfg ${configFile} \
+#         --port 60001
+#     '';
+#     path = [pkgs.kanata];
+#   };
+# }
